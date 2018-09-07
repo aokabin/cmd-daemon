4bc3510e3e74868b564c0fb29771af9a9c961b18
aokabin <info.aokabin@gmail.com>
2018-09-07 18:11:03 +0900

    Update to use open command and application

--- handler/handler.go
+++ handler/handler.go
@@ -3,16 +3,18 @@ package handler
 import (
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"strings"
 
 	"github.com/labstack/echo"
 )
 
 var (
-	cmd     = "open"
-	appPath = "/Applications/"
+	appDir = "/Applications/"
 )
 
 type ErrorStatus struct {
@@ -27,14 +29,18 @@ func Open(c echo.Context) error {
 		return c.String(errStatus.Code, errStatus.ErrorMessage.Error())
 	}
 
-	app := c.QueryParam("app")
 	args := []string{}
+	app := c.QueryParam("app")
 	if app != "" {
+		errStatus = appCheck(app)
+		if errStatus != nil {
+			return c.String(errStatus.Code, errStatus.ErrorMessage.Error())
+		}
 		args = append(args, "-a", app)
 	}
 	args = append(args, path)
 
-	errStatus = openCmd(cmd, args)
+	errStatus = openCmd(args)
 	if errStatus != nil {
 		return c.String(errStatus.Code, errStatus.ErrorMessage.Error())
 	}
@@ -60,7 +66,50 @@ func exists(path string) bool {
 	return err == nil
 }
 
-func openCmd(cmd string, args []string) *ErrorStatus {
+func appCheck(appName string) *ErrorStatus {
+	return findApp(appName)
+}
+
+func findApp(appName string) *ErrorStatus {
+	apps := appwalk(appDir)
+	appFile := fmt.Sprintf("%s.app", appName)
+	for _, app := range apps {
+		if app == appFile {
+			return nil
+		}
+	}
+	errStr := fmt.Sprintf("error occured: not found application: %v.", appName)
+	return &ErrorStatus{http.StatusBadRequest, errors.New(errStr)}
+}
+
+func appwalk(appdir string) []string {
+	if fileAccessDenyed(appdir) {
+		return nil
+	}
+
+	files, err := ioutil.ReadDir(appdir)
+	if err != nil {
+		panic(err)
+	}
+
+	var paths []string
+	for _, file := range files {
+		fileIsApp := strings.HasSuffix(file.Name(), ".app")
+		if fileIsApp {
+			paths = append(paths, file.Name())
+			continue
+		}
+		if file.IsDir() {
+			paths = append(paths, appwalk(filepath.Join(appdir, file.Name()))...)
+			continue
+		}
+	}
+
+	return paths
+}
+
+func openCmd(args []string) *ErrorStatus {
+	cmd := "open"
 	fmt.Println(cmd, args)
 	err := exec.Command(cmd, args...).Run()
 	if err != nil {
@@ -68,4 +117,13 @@ func openCmd(cmd string, args []string) *ErrorStatus {
 		return &ErrorStatus{http.StatusInternalServerError, errors.New(errStr)}
 	}
 	return nil
+}
+
+func fileAccessDenyed(filename string) bool {
+	info, err := os.Stat(filename)
+	if err != nil {
+		panic(err)
+	}
+	m := info.Mode()
+	return m&(1<<2) == 0
 }
